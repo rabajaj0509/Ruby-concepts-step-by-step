428c9835b5479cfc3df4aed01a4352c4c0a7021d
Rahul Bajaj <rahulrb0509@gmail.com>
2018-05-17 16:55:16 +0530

    Add tests for fibonacci series
    Chapter 1 Codes
    
    Chapter 2 Codes

---
+++ Akshay/Chapter2-Objects_Methods_and_LocalVariables/object_talk.rb
@@ -0,0 +1,7 @@
+obj = Object.new
+def obj.talk 
+	puts "I am an object." 
+	puts "(Do you object?)"
+end
+
+obj.talk
---
+++ Akshay/Chapter2-Objects_Methods_and_LocalVariables/string_replace.rb
@@ -0,0 +1,5 @@
+str = "Hello"
+abc = str
+str.replace("Goodbye")
+puts str
+puts abc
